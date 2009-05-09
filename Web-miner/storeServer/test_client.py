f091af71ace8cf168cd63278573722cbcdf60f9a
thugliffe <thugliffe@f202d41c-3a4b-11de-b438-597f59cd7555>
2009-05-09 10:51:39 +0000

    added author info

--- Web-miner/storeServer/test_client.py
+++ Web-miner/storeServer/test_client.py
@@ -1,3 +1,15 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+ 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+__author__ = " Biru C. Sainju"
+
+__version__ = "0.1"
 # Echo client program
 import my_gzip
 import zlib
--- Web-miner/test_PersistentDict.py
+++ Web-miner/test_PersistentDict.py
@@ -1,4 +1,16 @@
-from PersistentDefaultDict import Crawled
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+ 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+__author__ = " Biru C. Sainju"
+
+__version__ = "0.1"
+#from PersistentDefaultDict import Crawled
 from PersistentDefaultDict import crawled_urls
 print crawled_urls
 host='simpsons'
